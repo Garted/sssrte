b347c8cb3ac3f2900405805cd6d8d5c0bcef9f01
garted <garted93@gmail.com>
2023-09-27 17:40:48 +0300

    scr

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,10 @@
+"use strict";
+
+//СКРИПТ НА СЛАЙДЕР//
+
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 const slides = document.querySelectorAll(".slider__slide");
-
 let currentSlide = 0;
 // Функция для отображения текущего слайда
 function showSlide(index) {
@@ -26,9 +29,9 @@ prevBtn.addEventListener("click", () => {
 // Показываем первый слайд при загрузке страницы
 showSlide(currentSlide);
 
-// Получаем все элементы с классом .item
-const items = document.querySelectorAll(".catalog__elem");
+//СКРИПТ НА СЛАЙДЕР ОКОНЧАНИЕ//
 
+const items = document.querySelectorAll(".catalog__elem");
 // Добавляем обработчик события click к каждому элементу
 items.forEach((item) => {
   item.addEventListener("click", () => {
@@ -38,24 +41,61 @@ items.forEach((item) => {
     item.classList.add("catalog__elem_active");
   });
 });
-
 const cards = document.querySelectorAll(".catalog__item");
-
 cards.forEach((card) => {
   card.addEventListener("click", function () {
     this.classList.toggle("flipped");
   });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const showFitnessItems = document.querySelectorAll(".show-fitness");
-  const cards = document.querySelectorAll(".fitness");
+const wrap = document.querySelector(".catalog__wrap");
 
+document.addEventListener("DOMContentLoaded", function () {
+  const showFitnessItems = document.querySelectorAll(
+    ".catalog__elem.show-fitness"
+  );
   showFitnessItems.forEach(function (item) {
     item.addEventListener("click", function () {
       cards.forEach(function (card) {
         if (card.classList.contains("fitness")) {
           card.style.display = "block";
+          wrap.style.justifyContent = "flex-start";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const showRunningItems = document.querySelectorAll(
+    ".catalog__elem.show-running"
+  );
+  showRunningItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      cards.forEach(function (card) {
+        if (card.classList.contains("running")) {
+          card.style.display = "block";
+          wrap.style.justifyContent = "flex-start";
+        } else {
+          card.style.display = "none";
+        }
+      });
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const showTriatlonItems = document.querySelectorAll(
+    ".catalog__elem.show-triatlon"
+  );
+  showTriatlonItems.forEach(function (item) {
+    item.addEventListener("click", function () {
+      cards.forEach(function (card) {
+        if (card.classList.contains("triatlon")) {
+          card.style.display = "block";
+          wrap.style.justifyContent = "flex-start";
         } else {
           card.style.display = "none";
         }
